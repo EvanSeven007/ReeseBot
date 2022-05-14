8dcc44e8440e4def37f73cdfd918f69dcbdd60ec
EvanSeven007 <evanstegall123@gmail.com>
2022-05-14 03:19:22 -0500

    added more board to the board state

--- src/main.rs
+++ src/main.rs
@@ -85,76 +85,146 @@ impl Square {
 
 }
 
+struct position {
+    x: usize,
+    y: usize,
+}
 /* A board is a 8x8 array of squares */
-struct Board {
-    squares: [[Square; 8]; 8]
+struct BoardState {
+    squares: [[Square; 8]; 8],
+    active_color: Color, 
+    can_castle_white_kingside: bool,
+    can_casatle_white_queenside: bool,
+    can_castle_black_kingside: bool, 
+    can_castle_black_queenside: bool,
+    en_passant: Option<position>,
+    //Todo halfmove
+    //Todo full move
+    
 }
 
-impl Board {
+impl BoardState {
     /* Creating an empty board */
 
-    /* GOAL: TODO change this to support FEN NOTATION */ 
-    fn new() -> Board {
+    /* Creates a board state from a FEN string */
+    fn new(fen: &str) -> Result<BoardState, &str> {
         //Creating an 8x8 array of uninitialized arrays
-        let mut squares = [[Square {piece: None, color: (Color::Undef) }; 8]; 8]; 
-
-        /* All of the below code initialized a basic 8x8 board. It is very inelegant, and
-        the need for FEN notation is apparent */
-        let mut index1: u16;
-        let mut index2: u16;
+        let mut squares = [[Square {piece: None, color: (Color::Undef) }; 8]; 8];
+        //Assigning colors, but not charged
         for index in 0..8 {
-            //Getting the color of a piece if one exists on this square 
-            let piece_color = match index { 
-                0 | 1 => Color::White,
-                6 | 7 => Color::Black,
-                _ => Color::Undef, //Risky
-            };
+            for inner_index in 0..8 {
+                squares[index][inner_index].color = BoardState::get_color(&index, &inner_index);
+            }
+        }
+        let mut fen = fen.to_string();
+        //Stupid way to get rid of new line characters
+        if fen.ends_with('\n') {
+            fen.pop();
+            if fen.ends_with('\r') {
+                fen.pop();
+            }
+        }
 
-            match index {
-                //Populating first and back row
-                0 | 7 => {
-                    for inner_index in 0..8 {
-                        /* This is dumb */
-                        index1 = index.clone() as u16;
-                        index2 = inner_index.clone() as u16;
-                        let color = Board::get_color(index1, index2);
-                        let piece = match inner_index {
-                            0 | 7 => Piece{piece_type: PieceType::Rook, color: piece_color},
-                            1 | 6 => Piece{piece_type: PieceType::Knight, color: piece_color},
-                            2 | 5 => Piece{piece_type: PieceType::Bishop, color: piece_color},
-                            3 => Piece{piece_type: PieceType::Queen, color: piece_color},
-                            4 => Piece{piece_type: PieceType::King, color: piece_color},
-                            _ => {panic!("Not a valid piece type")}
-                        };
-                        squares[index][inner_index] = Square{ piece: Some(piece), color};
-                    }
-                }, 
-                //Matching pawn rows
-                1 | 6 => { 
-                    for inner_index in 0..8 {
-                        /* This is dumb */
-                        let piece = Piece{piece_type: PieceType::Pawn, color: piece_color};
-                        index1 = index.clone() as u16;
-                        index2 = inner_index.clone() as u16;
-                        let color = Board::get_color(index1, index2);
-                        squares[index][inner_index] = Square { piece: Some(piece), color};
-                    }
-                },
-                _ => {
-                    for inner_index in 0..8 {
-                        /* This is dumb */
-                        index1 = index.clone() as u16;
-                        index2 = inner_index.clone() as u16;
-                        squares[index][inner_index] = Square {piece : None, color: Board::get_color(index1, index2)};
-                    }
+        let fen_strings: Vec<&str> = fen.split(' ').collect();
+        if(fen_strings.len() != 6) {
+            return Err("Invalid fen string!");
+        }
+
+        println!("{}", fen_strings[0]);
+        let position_str: Vec<&str> = fen_strings[0].split('/').collect();
+        let mut col: usize;
+        let mut row_string: &str; //String that stores the current row info
+        for row in 0..8 {
+            row_string = position_str[row];
+            col = 0;
+            for fen_entry in row_string.chars() {
+                if fen_entry.is_digit(10) {
+                    col += (fen_entry.to_digit(10).unwrap() as usize);
+                } else {
+                    squares[row][col].piece = BoardState::parse_fen_entry(&fen_entry).unwrap();
+                    col += 1;
                 }
             }
         }
-        Board { squares }
+
+        let active_color = match fen_strings[1] {
+            "w" => Color::White,
+            "b" => Color::Black,
+            _ => panic!("fen string color malformed!"),
+        };
+
+        let mut can_castle_white_kingside = false;
+        let mut can_casatle_white_queenside = false;
+        let mut can_castle_black_kingside = false;
+        let mut can_castle_black_queenside = false;
+        /* This is unforgiveably stupid */
+        for fen_entry in fen_strings[2].chars() {
+            match fen_entry {
+                'K' => can_castle_white_kingside = true,
+                'Q' => can_casatle_white_queenside = true,
+                'k' => can_castle_black_kingside = true,
+                'q' => can_castle_black_queenside = true,
+                '-' => {},
+                _ => panic!("fen string castling info is malformed!"),
+            }
+        }
+        //Variables for enpassant goodness
+        let mut en_passant: Option<position>;
+        let x: usize;
+        let y: usize;
+        if fen_strings[3].len() == 1 && fen_strings[3] == "-" {
+            en_passant = None;
+        } else if fen_strings[3].len() == 2 { 
+            let en_passant_string: Vec<char> = fen_strings[3].chars().collect();
+            match en_passant_string[0] {
+                'a' => x = 0,
+                'b' => x = 1,
+                'c' => x = 2,
+                'd' => x = 3,
+                'e' => x = 4,
+                'f' => x = 5,
+                'g' => x = 6,
+                'h' => x = 7,
+                _ => panic!("fen string enpassant malformed!"),
+            };
+            /* This is just atrocious Evan, fix this */
+            if(!en_passant_string[1].is_digit(10)){
+                return Err("fen string enpassant malformed!")
+            }
+            y = (en_passant_string[1].to_digit(10).unwrap() - 1) as usize; /* Potential off by one bug */
+            if y > 7 {
+                return Err("fen string enpassant malformed!")
+            }
+            en_passant = Some(position{ x, y });
+        } else {
+            return Err("fen string enpassant malformed!")
+        }
+
+        Ok(BoardState { squares, active_color, can_castle_white_kingside, can_casatle_white_queenside, can_castle_black_kingside, can_castle_black_queenside, en_passant })
+    }
+
+
+    fn parse_fen_entry(entry: &char) -> Result<Option<Piece>, &str> {
+        match entry {
+            'r' => Ok(Some(Piece {piece_type: PieceType::Rook, color: Color::Black})),
+            'n' => Ok(Some(Piece {piece_type: PieceType::Knight, color: Color::Black})),
+            'b' => Ok(Some(Piece {piece_type: PieceType::Bishop, color: Color::Black})),
+            'q' => Ok(Some(Piece {piece_type: PieceType::Queen, color: Color::Black})),
+            'k' => Ok(Some(Piece {piece_type: PieceType::King, color: Color::Black})),
+            'p' => Ok(Some(Piece {piece_type: PieceType::Pawn, color: Color::Black})),
+            'R' => Ok(Some(Piece {piece_type: PieceType::Rook, color: Color::White})),
+            'N' => Ok(Some(Piece {piece_type: PieceType::Knight, color: Color::White})),
+            'B' => Ok(Some(Piece {piece_type: PieceType::Bishop, color: Color::White})),
+            'Q' => Ok(Some(Piece {piece_type: PieceType::Queen, color: Color::White})),
+            'K' => Ok(Some(Piece {piece_type: PieceType::King, color: Color::White})),
+            'P' => Ok(Some(Piece {piece_type: PieceType::Pawn, color: Color::White})),
+            _ => Err("Not a valid fen piece!"),
+        }
     }
     
+
     /* Gets the color of a board from its coordinates */
-    fn get_color(val1: u16, val2: u16) -> Color {
+    fn get_color(val1: &usize, val2: &usize) -> Color {
         match val1 {
             0 | 2 | 4 | 6 => {
                 match val2 {
@@ -172,7 +242,7 @@ impl Board {
     }
 
     fn print_board(&self) {
-        for index in (0..8).rev() {
+        for index in 0..8 {
             print!("[{}]", index + 1);
             for inner_index in 0..8 {
                 print!("{}", self.squares[index][inner_index].symbol());
@@ -183,16 +253,31 @@ impl Board {
     }
 }
 
+
 fn main() {
-    let board = Board::new();
-    //Add loop here, take in a move as input
-    //If the move is invalid, keep taking the move in
-    //Assess the move (is the game over or not?)
-    //Print the board after the move, 
-    //Loop
-    board.print_board();
-    //board.move(coord1, coord2)
-    //Have to make exceptions for castling 0 - 0 and pawn promotion e4 - e5Q
-    //Need to make functoin that checks if a king is in check given a board state and color of the king
-    //This is so we can do stuff like (the only valid moves are the one that get you out of checkl)
+    let board_state = "1R3b1r/p1ppkpp1/2n4n/4p3/p1b1P1B1/NpB2N2/PPP1rPPP/3QK2R b - - 0 1";
+    let board = BoardState::new(board_state);
+    match board {
+        Ok(_) => board.unwrap().print_board(),
+        Err(e) => println!("{}", e),
+    }
+    
+    //TODO
+    //Make a move parser
+    //takes in a string and returns a Result<Move>
+    
+    //Make a move struct
+    //This will consist of fields about a given move
+    //Move types can be: moving one piece from one square to another, 
+        //Subset of these are captures
+    //castles
+    //enpassant
+    //All moves can be checks
+    
+    //Move logic will ensure that the move is valid
+    //That is, the move is possible (i.e. not allowing a rook to skip over squares)
+    //Ensuring that a capture is indeed a capture (i.e. if the user writes exd5 then the program will ensure that a pawn exists that can take a pawn on d5)
+    //Ensuring the move is legal (not leaving a king in check)
+    //Probably many more edge cases
+    //
 }
